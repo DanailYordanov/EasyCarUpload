8a652bfc0505744388ccf05efde1fe677e2c3db2
Danail Yordanov <danail.yordanov1@gmail.com>
2021-07-21 15:51:55 +0300

    Divide the code into functions and remove time.sleep()

--- automoto_bg.py
+++ automoto_bg.py
@@ -1,49 +1,67 @@
-from selenium.webdriver.support.ui import Select
-from selenium import webdriver
-from decouple import config
+import os
 import time
 import pickle
+from decouple import config
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class images_uploaded():
+    """An expectation for checking that all images are uploaded"""
+
+    def __init__(self, locator, images_number):
+        self.locator = locator
+        self.images_number = images_number
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        images = element.find_elements_by_xpath('.//div[string-length(@id)>0]')
+        if len(images) == self.images_number:
+            return element
+        else:
+            return False
 
 
 class AutomotoBg():
 
     def __init__(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(30)
         self.load_cookies()
 
     def login(self):
 
         self.browser.get('https://automoto.bg/login')
 
-        time.sleep(5)
+        try:
+            cookie_consent_button = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, 'cc-btn')))
+            cookie_consent_button.click()
 
-        cookie_consent_button = self.browser.find_element_by_class_name(
-            'cc-btn')
-        cookie_consent_button.click()
+            email = config('AUTOMOTO_BG_EMAIL')
+            password = config('AUTOMOTO_BG_PASSWORD')
 
-        time.sleep(1)
+            email_input = self.browser.find_element_by_name('email')
+            email_input.send_keys(email)
 
-        email = config('AUTOMOTO_BG_EMAIL')
-        password = config('AUTOMOTO_BG_PASSWORD')
+            password_input = self.browser.find_element_by_name('password')
+            password_input.send_keys(password)
 
-        email_input = self.browser.find_element_by_name('email')
-        email_input.send_keys(email)
+            WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div/div/div[2]/section/form/div[2]/label/button')))
 
-        time.sleep(0.5)
+            self.browser.execute_script(
+                """$('button[type="submit"]').click();""")
 
-        password_input = self.browser.find_element_by_name('password')
-        password_input.send_keys(password)
+            time.sleep(5)
 
-        time.sleep(0.5)
-
-        button = self.browser.find_element_by_xpath(
-            '//*[@id="content"]/div/div/div[2]/section/form/div[2]/label/button')
-        button.click()
-
-        time.sleep(1)
-
-        pickle.dump(self.browser.get_cookies(),
-                    open('automoto_bg_cookies.pkl', 'wb'))
+            pickle.dump(self.browser.get_cookies(),
+                        open('automoto_bg_cookies.pkl', 'wb'))
+        except BaseException as e:
+            print(f'Something went wrong while logging you in! - {e}')
 
     def load_cookies(self):
         try:
@@ -53,124 +71,221 @@ class AutomotoBg():
             for cookie in cookies:
                 self.browser.add_cookie(cookie)
 
-            time.sleep(1)
-
             self.browser.refresh()
-
-            time.sleep(5)
-
-        except Exception:
+        except:
             self.login()
 
-    def add(self, category, brand, model, engine_type, price,
-            gear_type, fuel_type, power, cubature, year, month, run,
-            doors_type, color, euro_type, description, image_paths):
+    def choose_brand(self, choice):
+        try:
+            brand_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'mark_id')))
+            brand_select = Select(brand_select)
+            brand_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a brand!')
+
+    def choose_model(self, choice):
+        try:
+            model_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'model_id')))
+            model_select = Select(model_select)
+            model_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a model!')
+
+    def input_modification(self, input):
+        try:
+            modification_input = self.browser.find_element_by_name(
+                'modification')
+            modification_input.send_keys(input)
+        except:
+            print('Something went wrong while inputting a modification!')
+
+    def choose_category(self, choice):
+        try:
+            category_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'coupe_id')))
+            category_select = Select(category_select)
+            category_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a category!')
+
+    def input_price(self, input):
+        try:
+            price_input = self.browser.find_element_by_name('price')
+            price_input.send_keys(input)
+        except:
+            print('Something went wrong while inputting a price!')
+
+    def choose_transmission_type(self, choice):
+        try:
+            transmission_type_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'speed_id')))
+            transmission_type_select = Select(
+                self.browser.find_element_by_name('speed_id'))
+            transmission_type_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a transmission type!')
+
+    def choose_fuel_type(self, choice):
+        try:
+            fuel_type_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'fuel_id')))
+            fuel_type_select = Select(
+                self.browser.find_element_by_name('fuel_id'))
+            fuel_type_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a fuel type!')
+
+    def input_power(self, input):
+        try:
+            power_input = self.browser.find_element_by_name('power')
+            power_input.send_keys(input)
+        except:
+            print('Something went wrong while inputting power!')
+
+    def input_displacement(self, input):
+        try:
+            displacement_input = self.browser.find_element_by_name('cubic')
+            displacement_input.send_keys(input)
+        except:
+            print('Something went wrong while inputting displacement!')
+
+    def choose_month(self, choice):
+        try:
+            month_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'month_id')))
+            month_select = Select(
+                self.browser.find_element_by_name('month_id'))
+            month_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a month!')
+
+    def choose_year(self, choice):
+        try:
+            year_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'year_id')))
+            year_select = Select(self.browser.find_element_by_name('year_id'))
+            year_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a year!')
+
+    def input_run(self, input):
+        try:
+            run_input = self.browser.find_element_by_name('mileage')
+            run_input.send_keys(input)
+        except:
+            print('Something went wrong while inputting run!')
+
+    def choose_doors_type(self, choice):
+        try:
+            doors_type_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'door_id')))
+            doors_type_select = Select(
+                self.browser.find_element_by_name('door_id'))
+            doors_type_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a doors type!')
+
+    def choose_color(self, choice):
+        try:
+            color_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'color_id')))
+            color_select = Select(
+                self.browser.find_element_by_name('color_id'))
+            color_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a color!')
+
+    def choose_euro_standart(self, choice):
+        try:
+            euro_standart_select = WebDriverWait(self.browser, 30).until(
+                EC.element_to_be_clickable((By.NAME, 'euro_standart_id')))
+            euro_standart_select = Select(
+                self.browser.find_element_by_name('euro_standart_id'))
+            euro_standart_select.select_by_visible_text(choice)
+        except:
+            print('Something went wrong while choosing a EURO standart!')
+
+    def input_description(self, input):
+        try:
+            description_input = self.browser.find_element_by_id('description')
+            description_input.send_keys(input)
+        except:
+            print('Something went wrong while inputting a description!')
+
+    def upload_images(self, paths):
+        try:
+            # When you join all paths with \n you can pass them to the input at once
+            multiple_images_path = '\n'.join(paths)
+            image_input = self.browser.find_element_by_xpath(
+                '//input[@type="file"]')
+            image_input.send_keys(multiple_images_path)
+        except:
+            print('Something went wrong while uploading images!')
+
+    def add(self, category, brand, model, modification, price,
+            transmission_type, fuel_type, power, displacement, year, month, run,
+            doors_type, color, euro_standart, description, image_paths):
 
         self.browser.get('https://automoto.bg/listings/create')
 
-        time.sleep(3)
+        self.choose_brand(brand)
 
-        brand_select = Select(self.browser.find_element_by_name('mark_id'))
-        brand_select.select_by_visible_text(brand)
+        self.choose_model(model)
 
-        time.sleep(1)
+        self.input_modification(modification)
 
-        model_select = Select(self.browser.find_element_by_name('model_id'))
-        model_select.select_by_visible_text(model)
+        self.choose_category(category)
 
-        time.sleep(1)
+        self.input_price(price)
 
-        engine_type_input = self.browser.find_element_by_name('modification')
-        engine_type_input.send_keys(engine_type)
+        self.choose_transmission_type(transmission_type)
 
-        time.sleep(1)
+        self.choose_fuel_type(fuel_type)
 
-        category_select = Select(self.browser.find_element_by_name('coupe_id'))
-        category_select.select_by_visible_text(category)
+        self.input_power(power)
 
-        price_input = self.browser.find_element_by_name('price')
-        price_input.send_keys(price)
+        self.input_displacement(displacement)
 
-        time.sleep(1)
+        self.choose_month(month)
 
-        gear_type_select = Select(
-            self.browser.find_element_by_name('speed_id'))
-        gear_type_select.select_by_visible_text(gear_type)
+        self.choose_year(year)
 
-        time.sleep(1)
+        self.input_run(run)
 
-        fuel_type_select = Select(self.browser.find_element_by_name('fuel_id'))
-        fuel_type_select.select_by_visible_text(fuel_type)
+        self.choose_doors_type(doors_type)
 
-        time.sleep(1)
+        self.choose_color(color)
 
-        power_input = self.browser.find_element_by_name('power')
-        power_input.send_keys(power)
+        self.choose_euro_standart(euro_standart)
 
-        time.sleep(1)
+        self.input_description(description)
 
-        cubature_input = self.browser.find_element_by_name('cubic')
-        cubature_input.send_keys(cubature)
+        self.upload_images(image_paths)
 
-        time.sleep(1)
+        WebDriverWait(self.browser, 30).until(
+            images_uploaded((By.ID, 'images'), len(image_paths)))
 
-        month_select = Select(self.browser.find_element_by_name('month_id'))
-        month_select.select_by_visible_text(month)
-
-        time.sleep(1)
-
-        year_select = Select(self.browser.find_element_by_name('year_id'))
-        year_select.select_by_visible_text(year)
-
-        time.sleep(1)
-
-        run_input = self.browser.find_element_by_name('mileage')
-        run_input.send_keys(run)
-
-        time.sleep(1)
-
-        doors_type_select = Select(
-            self.browser.find_element_by_name('door_id'))
-        doors_type_select.select_by_visible_text(doors_type)
-
-        time.sleep(1)
-
-        color_select = Select(self.browser.find_element_by_name('color_id'))
-        color_select.select_by_visible_text(color)
-
-        time.sleep(1)
-
-        euro_type_select = Select(
-            self.browser.find_element_by_name('euro_standart_id'))
-        euro_type_select.select_by_visible_text(euro_type)
-
-        time.sleep(1)
-
-        description_input = self.browser.find_element_by_id('description')
-        description_input.send_keys(description)
-
-        time.sleep(1)
-
-        # When you join all paths with \n you can pass them to the input at once
-        multiple_images_path = '\n'.join(image_paths)
-        image_input = self.browser.find_element_by_xpath(
-            '//input[@type="file"]')
-        image_input.send_keys(multiple_images_path)
-
-        time.sleep(2)
-
-        button = self.browser.find_element_by_xpath(
-            '//*[@id="ID-listingOnlyCarCreate"]/div[24]/div/button')
+        button = WebDriverWait(self.browser, 30).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="ID-listingOnlyCarCreate"]/div[24]/div/button')))
         button.click()
 
 
+BASE_DIR = os.path.dirname(__file__)
+
 paths = [
-    'E:\Coding\CarsUpload\CarsUpload\pics\pic1.jpg',
-    'E:\Coding\CarsUpload\CarsUpload\pics\pic2.jpg',
-    'E:\Coding\CarsUpload\CarsUpload\pics\pic3.jpg',
-    'E:\Coding\CarsUpload\CarsUpload\pics\pic4.jpg',
-    'E:\Coding\CarsUpload\CarsUpload\pics\pic5.jpg',
-    'E:\Coding\CarsUpload\CarsUpload\pics\pic6.jpg'
+    os.path.join(BASE_DIR, '..\pics\pic1.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic2.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic3.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic4.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic5.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic6.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic7.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic8.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic9.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic10.jpg'),
+    os.path.join(BASE_DIR, '..\pics\pic11.jpg'),
 ]
 
 instance = AutomotoBg()
